028cdb3a5e76e32fd86907ce3571b7cc9e9d8c26
Matthew Zheng <lion@inssun.cn>
2023-07-06 16:50:40 +0800

    update Home

--- js/about.a2a3116a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunksocial_science"]=self["webpackChunksocial_science"]||[]).push([[443],{5399:function(n,s,e){e.r(s),e.d(s,{default:function(){return l}});var t=function(){var n=this;n._self._c;return n._m(0)},u=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],c=e(1001),i={},a=(0,c.Z)(i,t,u,!1,null,null,null),l=a.exports}}]);
-//# sourceMappingURL=about.a2a3116a.js.map
