2e061e454bb9d73c7b02e06693ac09bd86ba08f9
Matthew Zheng <lion@inssun.cn>
2023-07-07 23:29:51 +0800

    update Home

---
+++ js/769.fc2baccc.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkshedu_sheke"]=self["webpackChunkshedu_sheke"]||[]).push([[769],{5769:function(e,n,s){s.r(n),s.d(n,{default:function(){return o}});var t=function(){var e=this,n=e._self._c;return n("div",{staticClass:"home"},[n("banner"),n("News")],1)},u=[];const c=()=>s.e(758).then(s.bind(s,5758)),r=()=>s.e(238).then(s.bind(s,9238));var h={components:{Banner:c,News:r}},a=h,i=s(1001),l=(0,i.Z)(a,t,u,!1,null,"597ecc52",null),o=l.exports}}]);
+//# sourceMappingURL=769.fc2baccc.js.map
